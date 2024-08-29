8ad5d185f3f17c21069945bcc8ca4ab3b0338c54
Abduaizm <qweqwe@gmail.com>
2024-08-29 10:38:30 +0500

    save

---
+++ main.js
@@ -0,0 +1,79 @@
+var time = +prompt('введите время в цифрах')
+
+
+switch(time) {
+    case 0 :
+       alert(time + ' часа ночи')
+       break
+    case 1 :
+       alert(time + ' часа ночи')
+       break
+    case 2 :
+       alert(time + ' часа ночи')
+       break
+    case 3 :
+       alert(time + ' часа ночи')
+       break
+    case 4 :
+       alert(time + ' часа ночи')
+       break
+    case 5 :
+       alert(time + ' часа ночи')
+       break
+    case 6 :
+       alert(time + ' часа ночи')
+       break
+    case 7 :
+       alert(time + ' часов утра')
+       break   
+    case 8 :
+       alert(time + ' часов утра')
+       break   
+    case 9 :
+       alert(time + ' часов утра')
+       break   
+    case 10 :
+       alert(time + ' часов утра')
+       break   
+    case 11 :
+       alert(time + ' часа дня')
+       break   
+    case 12 :
+       alert(time + ' часа дня')
+       break   
+    case 13 :
+       alert(time + ' часа дня')
+       break 
+    case 14 :
+       alert(2 + ' часа дня')
+       break   
+    case 15 :
+       alert(3 + ' часа дня')
+       break   
+    case 16 :
+       alert(4 + ' часа дня')
+       break   
+    case 17 :
+       alert(5 + ' часа дня')
+       break   
+    case 18 :
+       alert(6 + ' часов вечера')
+       break   
+    case 19 :
+       alert(7 + ' часов вечера')
+       break  
+    case 20 :
+       alert(8 + ' часов вечера')
+       break   
+    case 21 :
+       alert(9 + ' часов вечера')
+       break   
+    case 22 :
+       alert(10 + ' часов вечера')
+       break   
+    case 23 :
+       alert(11 + ' часов вечера')
+       break   
+    case 24 :
+       alert(12 + ' часов вечера')
+}
